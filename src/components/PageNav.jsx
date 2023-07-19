6a65a3b1658a394e4eb8014ff223110c0de020ce
yaredow <yaredyilma11@gmail.com>
2023-07-19 11:38:13 +0300

    finish building the Navigation bar in all paes

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -5,8 +5,8 @@ import Logo from "../components/Logo";
 export default function PageNav() {
   return (
     <nav className={styles.nav}>
+      <Logo />
       <ul>
-        <Logo />
         <li>
           <NavLink to="/pricing">Pricing</NavLink>
         </li>
